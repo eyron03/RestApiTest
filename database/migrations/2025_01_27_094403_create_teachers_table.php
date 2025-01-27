eb0ed266bc58ae8bcc684e6d92a9867838977bb0
eyron03 <aaronchristianarenas50@gmail.com>
2025-01-27 17:54:57 +0800

    test

--- database/migrations/2025_01_27_094403_create_teachers_table.php
+++ database/migrations/2025_01_27_094403_create_teachers_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('teachers', function (Blueprint $table) {
             $table->id();
             $table->string('username');
             $table->string('password');
             $table->string('email')->unique();
             $table->string('name');
             $table->integer('age');
-            
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('teachers');
     }
 };
--- database/migrations/2025_01_27_094438_create_admins_table.php
+++ database/migrations/2025_01_27_094438_create_admins_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('username');
             $table->string('password');
-            $table->string('email')->unique();
-            $table->string('name');
-            $table->integer('age');
-            
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('admins');
     }
 };
